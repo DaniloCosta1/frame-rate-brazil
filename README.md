23b9e3cc6d77abe4c6109041ee01baa6fd0c9b92
DaniloCosta1 <frameratebrazil@gmail.com>
2025-04-08 19:18:32 -0300

    ok

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Site Oficial
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Site Oficial Frame Rate Brazil
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
@@ -8,7 +7,8 @@ function App() {
 
   return (
     <>
-      <h1>Frame Rate Brazil</h1>
+      <Header />
+      
     </>
   )
 }
