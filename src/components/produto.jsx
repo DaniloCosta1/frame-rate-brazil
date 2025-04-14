ffa602d17e3e6ed5f5f600ba5d8d59f069b3e834
DaniloCosta1 <frameratebrazil@gmail.com>
2025-04-14 08:43:16 -0300

    Create produto.jsx

---
+++ src/components/produto.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import listaDeProduto from '../assets/database/aliexpress.js'
+
+const produto = () => {
+  return (
+    <div className='produto'>
+      <h3>Produto</h3>
+      <img src="" alt="" />
+
+    </div>
+  )
+}
+
+export default produto
