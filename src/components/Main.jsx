3a67c9e7f74e95e65f168fa121e22d1877751f51
DaniloCosta1 <99129932+DaniloCosta1@users.noreply.github.com>
2025-06-05 10:02:10 -0300

    ok

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Sidebar from './Sidebar'
-import Maincointainer from './Maincointainer'
+
 
 const Main = () => {
   return (
     <div className='Main'>
-        <h2>main</h2>
+        
     </div>
   )
 }
